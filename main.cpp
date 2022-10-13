fe1edb7818bd4b771acd0fc3a260b32378bcfaeb
Frans <97081479+Fransssss@users.noreply.github.com>
2022-10-12 23:52:03 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+// Author: Fransiskus Agapa
+// Purpose: Practice Cstring - strlen - strcmp - strcpy
+// 10/12/22
+// Have Fun :)
+
+#include <iostream>
+#include "cstring"
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+int main()
+{
+    string choice;
+    cout << "== Practice Cstring ==" << endl;
+
+    char txt1[6] = "Rommo";
+    char txt2[6] = "Jully";
+
+    // check length
+    if(strlen(txt1) == strlen(txt2))
+    {
+        cout << "Your input have the same length " << strlen(txt1) << " characters" << endl;
+    }
+
+    // compare two item
+    if(strcmp(txt1, txt2) == 0)
+    {
+        cout << "Your both input are the same" << endl;
+    }
+
+    cout << endl << "Before copy [ " << txt1 << " ] to [ " << txt2 << " ]" <<  endl;
+    strcpy(txt1,txt2);
+    cout << endl << "After copy [ " << txt1 << "] [ " << txt2 << " ]" << endl;
+
+    return 0;
+}
